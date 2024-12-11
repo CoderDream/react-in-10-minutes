11e25eb81509a4e9eca9a67ca8469520d8237100
CoderDream <coderdream@gmail.com>
2024-12-11 11:41:33 +0800

    版本2：增加bootstrap样式

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,17 +1,49 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Button, Navbar, Modal } from "react-bootstrap";
+import { CardChecklist, Trash } from 'react-bootstrap-icons';
+import Container from 'react-bootstrap/Container';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+function fetchTodos() {
+  return [{ id: 1, title: "吃饭", completed: false },
+  { id: 2, title: "刷牙", completed: false },
+  { id: 3, title: "喝水", completed: true },
+  { id: 4, title: "洗澡", completed: false },
+  { id: 5, title: "睡觉", completed: true }];
+}
+
+function App() {
+  const todos = fetchTodos();
+
+  return <>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <CardChecklist /> 待办事项
+        </Navbar.Brand>
+      </Container>
+    
+    </Navbar>
+
+    <Container>
+      {
+        todos.map(todo => (<InputGroup key={todo.id}>
+          <InputGroup.Checkbox checked={todo.completed} />
+          <FormControl value={todo.title}
+            style={{
+              textDecoration: todo.completed ? 'line-through 4px' : 'none'
+            }}
+          />
+        </InputGroup>))
+      }
+    </Container>
+  </>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
